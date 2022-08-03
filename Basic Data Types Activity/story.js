c49cdab252f489edfc184ae1f363976b4a1112dc
nathanielg05 <nategayle05@gmail.com>
2022-08-03 11:22:14 -0400

    transfer

---
+++ Basic Data Types Activity/story.js
@@ -0,0 +1,39 @@
+//Create 4-string variables to describe the plot and basics of your story.
+//Create 3-integer variables to describe the elements in your story such as age, year, and century.
+//Create 1-array variable to demonstrate a collection of items or powers your character might have.
+//Create 1-boolean variable to demonstrate a true or false scenario.
+//Use the console to write and print your story. Remember to use the concatenation symbol ( + ) to combine your variables to strings.
+
+let greetings = "Welcome To my kingdom";
+let name ="Triva";
+let title = "Vintage ruler";
+let cityName = "Retropolis";
+
+let age = 30;
+let year = 40026;
+let century = 400;
+
+let itemHas = ["cup","Teapot","Gold coin","blanket"];
+
+let hasMoreItem = true;
+
+
+console.log(greetings + " My name is " + name + ".");
+console.log("I am the " + title + ".");
+console.log(" As the leader of " + cityName + " I love collecting old items" + ".");
+console.log(" Here are some of the items I've collected " + itemHas + ".");
+console.log(" Things become older faster on this planet. ");
+console.log(" I may be " + age + " but the year is " + year + " in the " + century + "th century.");
+console.log( " I have a lot of special items that have lasted for centuries.");
+console.log(" My favorite Item is this ancient " + itemHas[1] + " from 200 years ago" );
+console.log("Someone: Do you have more cool stuff?");
+console.log(" why thats " + hasMoreItem + ".");
+console.log(" You all can view all of these items and more in my grand gallery!");
+
+
+
+
+
+
+
+
